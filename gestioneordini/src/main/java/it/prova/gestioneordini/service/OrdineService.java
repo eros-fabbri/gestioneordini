eca1679f5da019837d5ba93c9a793d7ac2d694e8
erosfabbridev <erosfabbridev@gmail.com>
2022-05-27 09:22:08 +0200

    Update OrdineService.java

--- gestioneordini/src/main/java/it/prova/gestioneordini/service/OrdineService.java
+++ gestioneordini/src/main/java/it/prova/gestioneordini/service/OrdineService.java
@@ -1,4 +1,4 @@
-	package it.prova.gestioneordini.service;
+package it.prova.gestioneordini.service;
 
 import java.util.List;
 
@@ -8,7 +8,7 @@ import it.prova.gestioneordini.model.Categoria;
 import it.prova.gestioneordini.model.Ordine;
 
 public interface OrdineService {
-	
+
 	public List<Ordine> listAll() throws Exception;
 
 	public Ordine caricaSingoloElemento(Long id) throws Exception;
@@ -22,11 +22,11 @@ public interface OrdineService {
 	public void rimuovi(Long idArticolo) throws Exception;
 
 	public void aggiungiArticolo(Articolo articolo, Ordine ordine) throws Exception;
-	
+
 	public List<Ordine> findTuttiOrdiniDiUnaCategoria(Categoria categoria) throws Exception;
-		
+
 	public List<String> getIndirizziOrdiniCheHannoArticoliConCodiceCheContiene(String string) throws Exception;
-	
+
 	public Ordine findIlPiuRecenteDellaCategoria(Categoria categoria) throws Exception;
 
 	void setOrdineDAO(OrdineDAO ordineDAO);
